4f04606cadc1edb05b44c32b0356002e887a972e
Sonia Naranjo <sonianaranjo66@gmail.com>
2024-07-31 15:20:11 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
+
+let scene, camera, renderer
+
+function init() {
+    scene = new THREE.Scene
+
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth/window.innerHeight, 45, 30000)
+    camera.position.set(-900,-200,-900)
+
+    renderer = new THREE.WebGLRenderer({antialias:true})
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    const controls = new OrbitControls(camera, renderer.domElement)
+    controls.minDistance = 1
+    controls.maxDistance = 6000
+
+    let materialArray = []
+    let texture_ft = new THREE.TextureLoader().load('vulcan_ft.jpg')
+    let texture_bk = new THREE.TextureLoader().load('vulcan_bk.jpg')
+    let texture_up = new THREE.TextureLoader().load('vulcan_up.jpg')
+    let texture_dn = new THREE.TextureLoader().load('vulcan_dn.jpg')
+    let texture_rt = new THREE.TextureLoader().load('vulcan_rt.jpg')
+    let texture_lf = new THREE.TextureLoader().load('vulcan_lf.jpg')
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft}))
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_bk}))
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_up}))
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_dn}))
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_rt}))
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_lf}))
+    
+    for(let i=0; i<6;i++) {
+        materialArray[i].side = THREE.BackSide
+    }
+
+    let skyboxGeo = new THREE.BoxGeometry(10000,10000,10000)
+    let skybox = new THREE.Mesh(skyboxGeo, materialArray)
+    scene.add(skybox)
+    animate()
+}
+function animate() {
+    renderer.render(scene,camera)
+    requestAnimationFrame(animate)
+    controls.update()
+}
+init()
